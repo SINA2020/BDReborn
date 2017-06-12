96807c89535939ca867a171dfb98e868e83f38bc
SINA2020 <shomareh.majazi.nice@gmail.com>
2017-06-12 19:10:03 +0430

    Create bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -19,7 +19,7 @@ local notify = lgi.require('Notify')
 notify.init ("Telegram updates")
 chats = {}
 plugins = {}
-helper_id = 340155871 --Put Your Helper Bot ID Here
+helper_id = 334612749 --Put Your Helper Bot ID Here
 
 function do_notify (user, msg)
 	local n = notify.Notification.new(user, msg)
@@ -90,28 +90,28 @@ function create_config( )
 		"fun",
 
 	},
-    sudo_users = {111334847, 157059515, sudo_id},
+    sudo_users = {372530129, 378689087, sudo_id},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
-    info_text = [[》Beyond Reborn v6.0
+    info_text = [[》NICE Reborn v6.0
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/BeyondTeam/BDReborn 
+》 
 
 》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer《
-》@ToOfan ➣ Developer《
+》@shahin20nice ➣ Founder & Developer《
+》@MM20Nice ➣ Developer《
+》@Nice20Admin ➣ Developer《
 
 》Special thanks to :
-》Beyond Team Members
+》Nice Team Members
 
 》Our channel :
-》@BeyondTeam《
+》@Nice20Team《
 
-》Our website :
-》http://BeyondTeam.ir
+》  :
+》:
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
